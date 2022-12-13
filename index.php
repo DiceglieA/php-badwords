e217ea44d97827037c43b978a8ecacd93c0dc5a1
DiceglieA <angelodiceglie10@gmail.com>
2022-12-13 14:49:27 +0100

    last commit

--- index.php
+++ index.php
@@ -7,7 +7,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+    $p = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quo voluptates voluptatem possimus quae ex praesentium consequuntur, voluptatum modi! Quisquam, beatae consequuntur aliquam aut minima enim culpa quos sapiente aperiam?';
+    $badWord = $_GET['badWord'];
+    $censoredP = str_replace( $badWord, '***', $p );
+    ?>
 
+    <p><?php echo $p ?></p>
+    <h4>la lunghezza del paragrafo è di <?php echo strlen($p) ?> caratteri</h4>
+
+    <form action="" method="GET">
+        <div>
+            <label for="name">Bad Words</label>
+            <input type="text" name="badWord">
+        </div>
+        <button type="submit">INVIO</button>
+    </form>
+
+    <h1><?php echo $censoredP ?></h1>
+    <h2>la lunghezza del paragrafo è di <?php echo strlen($censoredP) ?> caratteri</h2>
 </body>
 </html>
